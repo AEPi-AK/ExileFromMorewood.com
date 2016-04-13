a454eb95c1ebf655d19579c840a94b441ac23f96
caroline hermans <carooolinehermans@gmail.com>
2016-04-13 03:55:51 -0400

    edit name screen updates with user input

--- app/js/EditNameView.jsx
+++ app/js/EditNameView.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 
 import EnterPlayerNumView from './EnterPlayerNumView.jsx'
-
 import '../less/EditNameView.less'
 
 class EditNameView extends React.Component {
@@ -10,7 +10,13 @@ class EditNameView extends React.Component {
         super(props)
         this.state = {
             playerNum: null,
+            playerName: 'lol some stuff idk lol',
         }
+    }
+
+    setName() {
+        const inputNode = ReactDOM.findDOMNode(this.refs.input)
+        this.setState({playerName: inputNode.value})
     }
 
     render() {
@@ -20,13 +26,18 @@ class EditNameView extends React.Component {
             />
         } 
         return (
-            <div className='container'>Edit Name 
-            Hallo Player Number {this.state.playerNum}
+            <div className='container'> 
+                <div className='title'>Edit Name</div> 
+                <div className='names-container'>
+                    <div className='name-title'>Current Name</div>
+                    <div className='name'>{this.state.playerName}</div>
+                    <div className='name-title'>New Name</div>
+                    <input type='text' ref='input' keyboardType='numeric'/>
+                    <div className='button' onClick={this.setName.bind(this)}>Finish</div>
+                </div>
             </div>
         )
     }
-
-
 }
 
 export default EditNameView
