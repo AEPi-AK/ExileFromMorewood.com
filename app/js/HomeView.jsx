21353b6719d136df31f1c026582f2a9ad27c8d5b
caroline hermans <carooolinehermans@gmail.com>
2016-04-14 00:34:49 -0400

    added responsive message telling laptop users to use their mobile phones or tablets instead

--- app/js/HomeView.jsx
+++ app/js/HomeView.jsx
@@ -9,13 +9,26 @@ class HomeView extends React.Component {
         {title: 'View Leaderboard', icon: 'trophy.svg', click:1},
         {title: 'View Character', icon: 'character.svg', click:2},
         {title: 'Edit Player Name', icon: 'edit.svg', click:3},
-        {title: 'Get Help', icon: 'help.svg', click:0},
+        // {title: 'Get Help', icon: 'help.svg', click:0},
     ].map(({title, icon, click}, i) => (
         <div className='button' onClick={this.props.setView.bind(this, click)} key={i}>
             <img src={`static/img/${icon}`}/>
             {title}
         </div>
     ))
+
+    if (window.innerWidth>=1024) {
+        console.log(`width is ${screen.width}`);
+        return (
+            <div className='laptop-display'>
+                <div className='laptop-logo'>
+                    <img src='static/img/dnd-logo-full.png'/>
+                </div>
+                <div className='laptop-text'> Please view our website on your 
+                smartphone or tablet. </div>
+            </div>
+        )
+    }
 
     return (
       <div className='container'>
